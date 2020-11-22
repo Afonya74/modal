8084e3c11f25df7d6c2155cf53d9cccefcd56c21
Afonya74 <67206390+Afonya74@users.noreply.github.com>
2020-11-22 19:18:11 +0100

    media query

--- modal.js
+++ modal.js
@@ -1,25 +1,55 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+ // Get the modal
+ var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+ // Get the button that opens the modal
+ var openModalBtn = document.getElementById("openModalBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+ // Get the <span> element that closes the modal
+ var closeModalSpan = document.getElementsByClassName("closeModal");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+ // When the user clicks the button, open the modal 
+ openModalBtn.onclick = function() {
+     modal.style.display = "block";
+ }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";;
-}
+ // When the user clicks on <span> (x), close the modal
+ closeModalSpan.onclick = function() {
+     modal.style.display = "none";
+ }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function(event) {
+     if (event.target == modal) {
+         modal.style.display = "none";
+     }
+ }
+
+ function closeModal() {
+     modal.style.display = "none";
+ }
+ /* 
+  function openModal(modal_id) {
+      var modalId = $('#' + modal_id);
+      modalId.fadeIn('slow');
+
+ 
+ }
+
+ function closeModal(modal_id) {
+     var modalId = $('#' + modal_id);
+     modalId.fadeOut('slow');
+
+     ;
+ }
+
+ window.onclick = function(event) {
+
+         switch (event.target.id) {
+             case 'myModal':
+                 $('#' + event.target.id).fadeOut('slow');
+                 break;
+             default:
+                 break;
+         }
+     } 
+      */
